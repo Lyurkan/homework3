e4a1ed1ddb01680033237a6c699773274ecb0398
Aleksandr.Bogolyubov <godoluboff_1990@mail.ru>
2023-03-29 12:50:13 +0300

    Initial commit

---
+++ src/HomeWork3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class HomeWork3 {
+    public static void main(String[] args) {
+        String[] questions = {"Сколько будет 2/2?", "Сколько будет 2*1?", "Сколько будет 2+1?"};
+        int[] correctAnswers = {1, 2, 3};
+        int[] userAnswers = new int[correctAnswers.length];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i]);
+            userAnswers[i] = scanner.nextInt();
+        }
+        int rightAnswers = 0;
+        for (int i = 0; i < correctAnswers.length; i++) {
+            if (correctAnswers[i] == userAnswers[i]) {
+                rightAnswers++;
+            }
+        }
+        System.out.println("Количество правильных ответов: " + rightAnswers + " из " + correctAnswers.length);
+    }
+}
