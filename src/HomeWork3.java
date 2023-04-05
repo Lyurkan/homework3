0ac76af5f63dab799ea824fda7e3cb89ccf434bc
Aleksandr.Bogolyubov <godoluboff_1990@mail.ru>
2023-04-05 17:53:48 +0300

    Выполненное третье домашнее задание (правки от 05.04.23)

--- src/HomeWork3.java
+++ src/HomeWork3.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 public class HomeWork3 {
     public static void main(String[] args) {
-        String[] questions = {"Сколько будет 2/2?", "Сколько будет 2*1?", "Сколько будет 2+1?"};
-        int[] correctAnswers = {1, 2, 3};
+        String[] questions = {"Сколько ног у собаки?\nВарианты ответов:\n1. Две ноги\n2. Три ноги\n3. Четыре ноги",
+                "В каком году началась вторая мировая война?\n1.1930\n2.1935\n3.1939\n4.1941",
+                "Кто основатель компании Microsoft?\n1. Гейтс\n2. Пакард\n3. Джобс\n4. Возняк\n5. Холлерит"};
+        int[] correctAnswers = {3, 3, 1};
         int[] userAnswers = new int[correctAnswers.length];
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < questions.length; i++) {
